05839fa4332481bd996155dd8e29ca05751f1ca3
GorkunovNM <gorkunov@physteh.edu>
2020-10-15 21:03:17 +0300

    Added example of testing

---
+++ main.cpp
@@ -0,0 +1,31 @@
+#include <cstdio>
+#include "my_sort/bubble_sort.cpp"      // 106425.139328 ms for 1e5 size
+#include "my_sort/insertion_sort.cpp"   // 63773.803891  ms for 1e5 size
+#include "my_sort/shell_sort.cpp"       // 227.231203    ms for 1e5 size
+#include "my_sort/heap_sort.cpp"        // 108298.404238 ms for 1e5 size
+#include "my_sort/merge_sort.cpp"       // 58.448836     ms for 1e5 size
+#include "my_sort/radix_sort.cpp"       // 108.331753    ms for 1e5 size
+#include "my_tester/sorts_tester.cpp"
+
+enum consts {
+    SIZE = 100000
+};
+
+int main(int argc, char const *argv[]) {
+    printf("Sorting vector of %d random unsigned integers.\n", SIZE);
+    my_tester::sort_ptr_tester sorts[] = {
+                                my_sort::bubble_sort   <my_tester::tester_it>, 
+                                my_sort::insertion_sort<my_tester::tester_it>, 
+                                my_sort::shell_sort    <my_tester::tester_it>, 
+                                my_sort::heap_sort     <my_tester::tester_it>, 
+                                my_sort::merge_sort    <my_tester::tester_it>, 
+                                my_sort::radix_sort    <my_tester::tester_it>};
+    double results[6];
+    my_tester::sorts_tester(results, sorts, 6, SIZE);
+    printf("bubble_sort:\t%lf ms\ninsertion_sort:\t%lf ms\nshell_sort:\t%lf ms\n"
+           "heap_sort:\t%lf ms\nmerge_sort:\t%lf ms\nradix_sort:\t%lf ms\n", 
+           results[0], results[1], results[2], 
+           results[3], results[4], results[5]);
+    printf("Test finished.\n");
+    return 0;
+}
